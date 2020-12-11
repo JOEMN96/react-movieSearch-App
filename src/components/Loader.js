f88e81dec45c186945573c21d862985c0cd8bbba
Joemn <Aruljoe37@gmail.com>
2020-12-11 05:59:05 +0530

    fixed redirect err & added meta to fix async calls

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,7 +3,7 @@ import React from "react";
 function Loader() {
   return (
     <div className=" loader--glisteningWindow">
-      <div class="loader"></div>
+      <div className="loader"></div>
     </div>
   );
 }
